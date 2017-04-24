5bd37570d959c510ce670c7f0620d252c016a5bc
Joe Hermaszewski <git@monoid.al>
2017-04-24 01:30:17 +0000

    Use fixed point hashes for arm bootstrap

--- pkgs/stdenv/linux/bootstrap-files/armv7l.nix
+++ pkgs/stdenv/linux/bootstrap-files/armv7l.nix
@@ -1,12 +1,12 @@
 {
   busybox = import <nix/fetchurl.nix> {
-    url = file:///nix/store/2bc6v8nlg9flynsyfzm1pg7x5kxwk35p-busybox-1.26.2/bin/busybox;
+    url = file:///nix/store/xdji4v69zqk5g36vnh1555zn9lskylv9-busybox-1.26.2/bin/busybox;
     sha256 = "1d8aaz3p45x4yvhzbvhj78bhf8p2vchwhir9zsrng03192pyh7hb";
     executable = true;
   };
 
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = file:///nix/store/l7hdd0b5dvdyy8nzcz6qhv9l89nxsqp9-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
+    url = file:///nix/store/pwgffnimf2n0drain6s23b94dgrr9z3d-stdenv-bootstrap-tools/on-server/bootstrap-tools.tar.xz;
     sha256 = "1xq581hlrs5lw7cdqy278zvvcbfy3d1cj5vrsc42jlvqy1msj3x2";
   };
 }
