6ab954928a3dff2482e7ed7f315b0b0b43007a57
Joe Hermaszewski <git@monoid.al>
2020-12-18 11:34:09 +0800

    monado: Add serviceSupport option
    This controls XRT_FEATURE_SERVICE. See
    https://gitlab.freedesktop.org/monado/monado/-/blob/master/doc/targets.md#xrt_feature_service-disabled
    for more info.

--- pkgs/development/libraries/monado/default.nix
+++ pkgs/development/libraries/monado/default.nix
@@ -1,7 +1,12 @@
 { stdenv, fetchFromGitLab, writeText, cmake, doxygen, glslang, pkgconfig
 , python3, SDL2, dbus, eigen, gst-plugins-base, gstreamer, hidapi, libXrandr
 , libav, libjpeg, libsurvive, libusb, libuv, opencv, openhmd, vulkan-headers
-, vulkan-loader, wayland, wayland-protocols, fetchpatch }:
+, vulkan-loader, wayland, wayland-protocols, fetchpatch
+# Set as 'false' to build monado without service support, i.e. allow VR
+# applications linking against libopenxr_monado.so to use OpenXR standalone
+# instead of via the monado-service program. For more information see:
+# https://gitlab.freedesktop.org/monado/monado/-/blob/master/doc/targets.md#xrt_feature_service-disabled
+, serviceSupport ? true }:
 
 stdenv.mkDerivation rec {
   pname = "monado";
@@ -26,7 +31,10 @@ stdenv.mkDerivation rec {
 
   nativeBuildInputs = [ cmake doxygen glslang pkgconfig python3 ];
 
-  cmakeFlags = [ "-DXRT_BUILD_DRIVER_SURVIVE=On" ];
+  cmakeFlags = [
+    "-DXRT_BUILD_DRIVER_SURVIVE=On"
+    "-DXRT_FEATURE_SERVICE=${if serviceSupport then "ON" else "OFF"}"
+  ];
 
   buildInputs = [
     SDL2
